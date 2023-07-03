f32da4040cfc9efbccf28b41d6f24f6dbc86594a
LinkN0W <44440387+LinkN0W@users.noreply.github.com>
2023-07-03 18:06:36 +0400

    Add files via upload

---
+++ superbook1/src/main/java/ru/team/superbook1/Superbook1Application.java
@@ -0,0 +1,13 @@
+package ru.team.superbook1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Superbook1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Superbook1Application.class, args);
+	}
+
+}
---
+++ superbook1/src/test/java/ru/team/superbook1/Superbook1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.team.superbook1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Superbook1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
